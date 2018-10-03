ff0445d39a96990f6d7d4c72f2d6d914a4882bbe
Tejas Dinkar <tejas@gja.in>
2018-10-03 22:16:51 +0530

    Authenticator takes a config

--- src/clj_totp/core.clj
+++ src/clj_totp/core.clj
@@ -1,10 +1,15 @@
 (ns clj-totp.core
   (:import [com.warrenstrange.googleauth
             GoogleAuthenticator
+            GoogleAuthenticatorConfig$GoogleAuthenticatorConfigBuilder
             GoogleAuthenticatorKey
             GoogleAuthenticatorQRGenerator]))
 
 (set! *warn-on-reflection* true)
+
+(def ^{:dynamic true :private true}
+  ^GoogleAuthenticator authenticator
+  (new GoogleAuthenticator))
 
 (defn- key->qr-code-url [^GoogleAuthenticatorKey key issuer account-name]
   (GoogleAuthenticatorQRGenerator/getOtpAuthURL issuer account-name key))
@@ -12,7 +17,7 @@
 (defn generate-key
   "Returns a newly generated key"
   [issuer account-name]
-  (let [key (.createCredentials (new GoogleAuthenticator))]
+  (let [key (.createCredentials authenticator)]
     {:secret-key (.getKey key)
      :qr-code-url (key->qr-code-url key issuer account-name)
      :scratch-codes (into [] (.getScratchCodes key))}))
@@ -20,13 +25,24 @@
 (defn valid-code?
   "Validates a code"
   ([^String secret-key ^long code]
-   (.authorize (new GoogleAuthenticator) secret-key code))
+   (.authorize authenticator secret-key code))
   ([^String secret-key ^long code ^long time]
-   (.authorize (new GoogleAuthenticator) secret-key code time)))
+   (.authorize authenticator secret-key code time)))
 
 (defn generate-otp
-  "Generates a TOTP Password. For Testing Only"
+  "Generates a TOTP Password. For sending via email/sms"
   ([^String secret-key]
-   (.getTotpPassword (new GoogleAuthenticator) secret-key))
+   (.getTotpPassword authenticator secret-key))
   ([^String secret-key ^long time]
-   (.getTotpPassword (new GoogleAuthenticator) secret-key time)))
+   (.getTotpPassword authenticator secret-key time)))
+
+(defmacro with-authenticator [the-authenticator & body]
+  `(binding [authenticator ~the-authenticator]
+     ~@body))
+
+(defn ^GoogleAuthenticator build-authenticator [& {:keys [time-step window-size code-digits]}]
+  (let [builder (cond-> (new GoogleAuthenticatorConfig$GoogleAuthenticatorConfigBuilder)
+                  time-step (.setTimeStepSizeInMillis time-step)
+                  window-size (.setWindowSize window-size)
+                  code-digits (.setCodeDigits code-digits))]
+    (new GoogleAuthenticator (.build builder))))
--- test/clj_totp/core_test.clj
+++ test/clj_totp/core_test.clj
@@ -10,3 +10,10 @@
     (is (not (valid-code? secret-key (inc code))))
 
     (is (not (valid-code? secret-key code (- (System/currentTimeMillis) 90000))))))
+
+(deftest it-can-take-different-configs
+  (with-authenticator (build-authenticator :code-digits 8)
+    (let [{:keys [secret-key]} (generate-key "Testing" "test@example.com")
+          code (generate-otp secret-key)]
+      (is (valid-code? secret-key code))
+      (is (<= 1000000 code 99999999)))))
